90e21ad7e2d95e83eb2b103ec1cf15c35334d129
SudipVikram <sudip.beyondapogee@gmail.com>
2025-03-27 19:42:11 +0545

    img corresponding to fingers

--- Testing.py
+++ Testing.py
@@ -4,7 +4,7 @@ from SajiloCV import *
 sajilo = SajiloCV()
 htracker = sajilo.HandTracking(sajilo)
 
-htracker.update_max_hands(2)
+htracker.update_max_hands(1)
 
 # instantiating tools
 tools = sajilo.Tools(sajilo,htracker)
@@ -12,11 +12,11 @@ tools = sajilo.Tools(sajilo,htracker)
 
 while True:
     htracker.track_hands()
-    #htracker.show_hand_connections()
-    htracker.line_across_landmarks()
+    htracker.show_hand_connections()
+    #htracker.line_across_landmarks()
 
     # creating a slider
-    htracker.draw_rectangle(fill=False)
+    '''htracker.draw_rectangle(fill=False)
     length = htracker.length_across_landmarks()
     if length:
         range_val = tools.find_range(length,min=150,max=400,lmin=20,lmax=100,order="descending")
@@ -26,13 +26,28 @@ while True:
         htracker.draw_vertical_slider(val=range_val)
         disp_range_val = tools.find_range(length, min=0, max=100, lmin=20, lmax=100, order="ascending")
         htracker.display_text(text=f"{int(disp_range_val)}%",org=(30,440),font="duplex", font_scale=1,color=(0,255,0),thickness=1)
-
+    '''
     # working with files in a dir
     #tools.print_dir_list(dir_path="fingers")
 
     #fileList = tools.find_dir_list(dir_path="fingers")
-    list_of_overlays = tools.load_images_from_dir(dir_path="fingers")
+    '''list_of_overlays = tools.load_images_from_dir(dir_path="fingers")
     #print(list_of_overlays)
-    tools.slice_image(img_num=6)
+    tools.slice_image(img_num=1)'''
+
+    ''' finding the right image for the number of fingers tracked '''
+    #htracker.print_landmarks()
+    #htracker.find_hand_position(draw=False)
+
+    ''' getting an image corresponding to the number of fingers'''
+    fingers = htracker.determine_hand_position()
+    tools.load_images_from_dir(dir_path="fingers")
+    if fingers:
+        #print(fingers)
+        thumb, index, middle, ring, pinky = fingers
+        if thumb == 1:
+            tools.slice_image(img_num=0)
+        elif index == 1:
+            tools.slice_image(img_num=1)
 
     htracker.display_video()
