d945d82b5a4fa7c0c9b71dfaf3e2261da4b4b826
Sudip Vikram Adhikari <32861861+SudipVikram@users.noreply.github.com>
2025-04-11 04:30:20 +0545

    created file finger counter inside examples.

---
+++ examples/basic hand tracking.py
@@ -0,0 +1,21 @@
+'''
+@author: Sudip Vikram Adhikari
+@company: Beyond Apogee
+This is a basic working example of sajilocv's hand tracking. Just the bare minimum code to get things working.
+'''
+
+from sajilocv import *
+
+# instantiating
+sajilo = SajiloCV()
+htracker = sajilo.HandTracking(sajilo)
+
+while True:
+    # start tracking hands
+    htracker.track_hands()
+
+    # show hand landmarks along with its connections
+    htracker.show_hand_connections()
+
+    # display the video output
+    htracker.display_video()
---
+++ examples/finger counter.py
@@ -0,0 +1,55 @@
+'''
+@author: Sudip Vikram Adhikari
+@company: Beyond Apogee
+This example demonstrates how to count the number of fingers in a hand using sajilocv.
+'''
+
+from sajilocv import *
+
+# instantiating
+sajilo = SajiloCV()
+htracker = sajilo.HandTracking(sajilo)
+
+# allowing only single hand tracking    # it is actually set to one hand by default, nonetheless
+htracker.update_max_hands(1)
+
+# instantiating tools class     # we will need this to overlay an external image
+tools = sajilo.Tools(sajilo,htracker)
+
+while True:
+    # start tracking hands
+    htracker.track_hands()
+
+    # getting an image corresponding to the number of fingers  # currently only tracks the right hand
+    fingers = htracker.determine_hand_position()    # this will give us the state of all five fingers
+    tools.load_images_from_dir(dir_path="assets")
+    if fingers:
+        print(fingers)
+        htracker.draw_rectangle(fill=True, color=(255, 255, 255), start=(147, 215), end=(180, 245))
+        htracker.display_text(text="fingers:", org=(30, 240), font="simplex", font_scale=1, color=(255, 0, 0),
+                              thickness=2)
+        thumb, index, middle, ring, pinky = fingers
+        if fingers == [0, 0, 0, 0, 0]:
+            tools.overlay_image(org=(10, 10), img_num=0)
+            htracker.display_text(text="0", org=(150, 240), font="duplex", font_scale=1, color=(0, 255, 0), thickness=2)
+        elif fingers == [0, 1, 0, 0, 0]:
+            tools.overlay_image(org=(10, 10), img_num=1)
+            htracker.display_text(text="1", org=(150, 240), font="duplex", font_scale=1, color=(0, 255, 0), thickness=2)
+        elif fingers == [0, 1, 1, 0, 0]:
+            tools.overlay_image(org=(10, 10), img_num=2)
+            htracker.display_text(text="2", org=(150, 240), font="duplex", font_scale=1, color=(0, 255, 0), thickness=2)
+        elif fingers == [0, 1, 1, 1, 0]:
+            tools.overlay_image(org=(10, 10), img_num=3)
+            htracker.display_text(text="3", org=(150, 240), font="duplex", font_scale=1, color=(0, 255, 0), thickness=2)
+        elif fingers == [0, 1, 1, 1, 1]:
+            tools.overlay_image(org=(10, 10), img_num=4)
+            htracker.display_text(text="4", org=(150, 240), font="duplex", font_scale=1, color=(0, 255, 0), thickness=2)
+        elif fingers == [1, 1, 1, 1, 1]:
+            tools.overlay_image(org=(10, 10), img_num=5)
+            htracker.display_text(text="5", org=(150, 240), font="duplex", font_scale=1, color=(0, 255, 0), thickness=2)
+
+    # show hand landmarks along with its connections
+    htracker.show_hand_connections()
+
+    # display the video output
+    htracker.display_video()
